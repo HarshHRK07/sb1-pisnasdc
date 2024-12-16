c5abd4260fe2b005780d70c09fb663323076ab5f
HarshHRK07 <git@stackblitz.com>
2024-12-16 08:27:46 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sb1-pisnasdc
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/HarshHRK07/sb1-pisnasdc)
---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Pricing from './pages/Pricing';
+import OrderNow from './pages/OrderNow';
+import Contact from './pages/Contact';
+import Blog from './pages/Blog';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Toaster position="top-center" />
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/order" element={<OrderNow />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
